7225c81e3017543f1aa5ae2989b176d6191225f4
plantvsbirds <plantvsbird@gmail.com>
2016-09-12 00:04:02 -0500

    init with vue, bootstrap, sass, top nav bar, placeholder

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-default hackx">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="logo" href="#">
+            <place-holder
+              width="80"
+              height="50"
+              text="hackx"
+            >
+            </place-holder>
+          </a>
+        </div>
+        <ul class="nav navbar-nav pull-right">
+          <li>
+            <a href="../getting-started/">
+              创客马拉松
+            </a>
+          </li>
+          <li>
+            <a href="../getting-started/">
+              开放职位
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <i class="glyphicon glyphicon-user">
+              </i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+    </div>
+  </div>
+</template>
+
+<script>
+
+import PlaceHolder from "./components/placeholder.vue"
+export default {
+  components: {
+    PlaceHolder
+  }
+}
+</script>
+
+<style lang="sass">
+@import './styles/defaults.scss'
+
+body
+  background: $bg-color
+
+nav.navbar.hackx
+  background: $theme-color
+
+nav.navbar.hackx > *
+  overflow: hidden
+
+nav.navbar.hackx ul.navbar-nav a
+  color: white
+  padding: 15px 25px
+  font-size: .9em
+  &:hover
+    color: white
+    background: darken($theme-color, 5%)
+
+</style>
---
+++ src/components/placeholder.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img src="https://placeholdit.imgix.net/~text?txtsize=20&txt={{text}}&w={{width}}&h={{height}}">
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['width', 'height', 'text']
+}
+</script>
+
+<style lang="sass" scoped>
+div
+  width: 100%
+  height: 100%
+</style>
